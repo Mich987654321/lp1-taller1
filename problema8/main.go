8856aaa111093538798881aa5fde6f00bc87c623
Danna <caicedovalenciadannamichelle@gmail.com>
2026-02-10 23:47:06 +0100

    Simulación de trabajo - problema 8

--- problema8/main.go
+++ problema8/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
 // Objetivo: Simular "futuros" en Go usando canales. Una función lanza trabajo asíncrono
 // y retorna un canal de solo lectura con el resultado futuro.
-// TODO: completa las funciones y experimenta con varios futuros a la vez.
+//completa las funciones y experimenta con varios futuros a la vez.
 
 func asyncCuadrado(x int) <-chan int {
 	ch := make(chan int)
 	go func() {
 		defer close(ch)
-		// TODO: simular trabajo
+		time.Sleep(time.Duration(x) * 100 * time.Millisecond)
+		//simular trabajo
 
 		ch <- x * x
 	}()
@@ -25,9 +25,8 @@ func main() {
 
 	// TODO: Opción 1: esperar cada futuro secuencialmente
 
-	
 	// TODO: Opción 2: fan-in (combinar múltiples canales)
 	// Pista: crea una función fanIn que recibe múltiples <-chan int y retorna un único <-chan int
 	// que emita todos los valores. Requiere goroutines y cerrar el canal de salida cuando todas terminen.
-	
+
 }
